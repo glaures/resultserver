0f1698c6d9a957808a41f15bcf9734bf1a36cd98
Guido Laures <guido@spreadshirt.net>
2018-10-07 15:32:28 +0200

    Initial commit

---
+++ src/main/java/de/sandkastenliga/resultserver/ResultserverApplication.java
@@ -0,0 +1,12 @@
+package de.sandkastenliga.resultserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultserverApplication.class, args);
+	}
+}
