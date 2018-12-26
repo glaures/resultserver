74752919130ab4fc62665f7536af9016f3c66393
Guido Laures <guido@spreadshirt.net>
2018-12-26 18:56:49 +0100

    enabled scheduling

--- src/main/java/de/sandkastenliga/resultserver/ResultserverApplication.java
+++ src/main/java/de/sandkastenliga/resultserver/ResultserverApplication.java
@@ -2,8 +2,10 @@ package de.sandkastenliga.resultserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ResultserverApplication {
 
 	public static void main(String[] args) {
